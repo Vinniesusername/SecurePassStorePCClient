7c159e1eae33da69b297db463d2adeaf7c510752
Vinnie <vprestianni@luc.edu>
2020-04-27 21:23:59 -0500

    added login screen to client

---
+++ src/com/SecurePassStore/Client/Client.java
@@ -0,0 +1,10 @@
+package com.SecurePassStore.Client;
+
+public class Client
+{
+    public static void main(String[] args)
+    {
+        LogIn login = new LogIn();
+        login.setVisible(true);
+    }
+}
--- src/com/SecurePassStore/Client/LogIn.java
+++ src/com/SecurePassStore/Client/LogIn.java
@@ -1,4 +1,38 @@
 package com.SecurePassStore.Client;
 
-public class LogIn {
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+public class LogIn extends JFrame
+{
+
+    private JPanel rootPanel;
+    private JButton logInButton;
+    private JLabel usernameLabel;
+    private JPasswordField passwordField;
+    private JLabel passwordLabel;
+    private JTextField usernameField;
+    private JButton loginButton;
+
+    public LogIn()
+    {
+        add(rootPanel);
+        setSize(250, 150);
+        setLocationRelativeTo(null);
+        setTitle("Secure Password Store 0.1");
+
+
+        logInButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent)
+            {
+                System.out.println("button pressed");
+
+            }
+        });
+    }
+
 }
