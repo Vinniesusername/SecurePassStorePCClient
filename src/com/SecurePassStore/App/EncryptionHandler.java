ceec18f57b34f17d640aece003f99e7778dc14b8
Vinnie <vprestianni@luc.edu>
2020-05-28 17:58:53 -0500

    added support for encrypting passwords.

--- src/com/SecurePassStore/App/EncryptionHandler.java
+++ src/com/SecurePassStore/App/EncryptionHandler.java
@@ -1,18 +1,45 @@
 package com.SecurePassStore.App;
+
 //TODO implement methods
+import javax.crypto.*;
+import javax.crypto.spec.SecretKeySpec;
+
 public class EncryptionHandler
 {
     private byte[] key = null;
     private String localKey = "";
+    private SecretKeySpec secretKeySpec;
+    private static EncryptionHandler encryptionHandler;
 
-    public EncryptionHandler(byte[] k)
+    private EncryptionHandler(byte[] k)
     {
         key = k;
+        secretKeySpec = new SecretKeySpec(key, "AES" );
+    }
+
+    public static EncryptionHandler getInstance(byte[] key)
+    {
+        if(encryptionHandler == null)
+            encryptionHandler = new EncryptionHandler(key);
+        return encryptionHandler;
+
     }
 
     public byte[] encryptPassword(byte[] password)
     {
-        return null;
+        byte[] encrypted = null;
+        try
+        {
+            Cipher c = Cipher.getInstance("AES");
+            c.init(Cipher.ENCRYPT_MODE, secretKeySpec);
+            encrypted = c.doFinal(password);
+        }
+        catch (Exception e)
+        {
+            System.out.println(e);
+        }
+
+        return encrypted;
     }
 
     public byte[] decryptPassword(byte[] password)
